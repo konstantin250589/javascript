16968addf0c3d56943abf99f673c5d561a0f42ec
Kostyan <duhinkonstantin@mail.ru>
2021-04-15 17:48:57 +0700

    20 - Object

--- js/script.js
+++ js/script.js
@@ -7,12 +7,22 @@ const options = {
     color: {
         border: 'black',
         bg: 'red'
+    },
+    makeTest: function () {
+        console.log('Test');
     }
 };
-// console.log(options.name);
 
-// delete options.name;
-// console.log(options);
+options.makeTest();
+
+const {border, bg} = options.color;
+console.log(border);
+
+console.log(Object.keys(options).length);
+console.log(options.name);
+
+delete options.name;
+console.log(options);
 let counter = 0;
 for (let key in options) {
     if (typeof(options[key]) === 'object') {
