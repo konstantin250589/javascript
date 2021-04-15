1deddd2154508740a360f28a406735f6187f313e
Kostyan <duhinkonstantin@mail.ru>
2021-04-15 18:56:08 +0700

    21 Array

--- js/script.js
+++ js/script.js
@@ -1,40 +1,35 @@
 'use strict';
 
-const options = {
-    name: 'test',
-    width: 1024,
-    height: 1024,
-    color: {
-        border: 'black',
-        bg: 'red'
-    },
-    makeTest: function () {
-        console.log('Test');
-    }
-};
+const arr = [1, 24, 15, 8, 5];
+arr.sort(compareNub);
+console.log(arr);
 
-options.makeTest();
-
-const {border, bg} = options.color;
-console.log(border);
-
-console.log(Object.keys(options).length);
-console.log(options.name);
-
-delete options.name;
-console.log(options);
-let counter = 0;
-for (let key in options) {
-    if (typeof(options[key]) === 'object') {
-        for (let i in options[key]) {
-            console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
-
-        } 
-    }
-          else {
-            console.log(`Свойство ${key} имеет значение ${options[key]}`);
-            counter++;
-        }
+function compareNub(a, b) {
+    return a - b;
 }
-console.log(counter);
+arr[99] = 0;
+
+console.log(arr.length);
+console.log(arr);
+arr.forEach(function(item, i, arr) {
+    console.log(`${i}: ${item} внутри массива ${arr}`);
+});
+
     
+arr.pop();
+arr.push(10);
+
+console.log(arr);
+
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
+
+for (let value of arr) {
+    console.log(value);
+}
+
+const str = prompt('', '');
+const product = str.split(", ");
+product.sort();
+console.log(product.join("; "));
