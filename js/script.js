ad8a4edfcabd79c475f4031e86b953c4c67c1ad0
Kostyan <duhinkonstantin@mail.ru>
2021-04-19 01:39:52 +0700

    22 Spread

--- js/script.js
+++ js/script.js
@@ -1,35 +1,132 @@
-'use strict';
+// 'use strict';
 
-const arr = [1, 24, 15, 8, 5];
-arr.sort(compareNub);
-console.log(arr);
+// const arr = [1, 24, 15, 8, 5];
+// arr.sort(compareNub);
+// console.log(arr);
 
-function compareNub(a, b) {
-    return a - b;
-}
-arr[99] = 0;
+// function compareNub(a, b) {
+//     return a - b;
+// }
+// arr[99] = 0;
 
-console.log(arr.length);
-console.log(arr);
-arr.forEach(function(item, i, arr) {
-    console.log(`${i}: ${item} внутри массива ${arr}`);
-});
+// console.log(arr.length);
+// console.log(arr);
+// arr.forEach(function(item, i, arr) {
+//     console.log(`${i}: ${item} внутри массива ${arr}`);
+// });
 
     
-arr.pop();
-arr.push(10);
+// arr.pop();
+// arr.push(10);
 
-console.log(arr);
+// console.log(arr);
 
-for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i]);
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// for (let value of arr) {
+//     console.log(value);
+// }
+
+// const str = prompt('', '');
+// const product = str.split(", ");
+// product.sort();
+// console.log(product.join("; "));
+
+
+// let a = 5;
+//     b = a;
+
+// b = b + 5;
+
+// console.log(b);
+// console.log(a);
+
+// const obj = {
+//     a: 5,
+//     b: 1
+// };
+
+// const copy = obj; //ссылка
+
+// copy.a = 10;
+
+// console.log(copy);
+// console.log(obj);
+
+function copy(mainObj) {
+    let copyObj = {};
+
+    let key;
+    for (key in mainObj) {
+        copyObj[key] = mainObj[key];
+    }
+
+    return copyObj;
 }
 
-for (let value of arr) {
-    console.log(value);
+const numbers = {
+    a: 2,
+    b: 5,
+    c: {
+        x: 7,
+        y: 4
+    }
+};
+
+
+const newNumbers = copy(numbers);
+
+newNumbers.a = 10;
+newNumbers.c.x = 10;
+
+// console.log(newNumbers);
+// console.log(numbers);
+
+const add = {
+    d: 17,
+    e: 20
+};
+
+const clone = (Object.assign({}, add));
+
+clone.d = 20;
+
+// console.log(add);
+// console.log(clone);
+
+const oldArray = ['a', 'b', 'c'];
+const newArray = oldArray.slice();
+
+newArray[1] = 'dsadas';
+
+console.log(oldArray);
+console.log(newArray);
+
+const video = ['youtube', 'vimeo', 'rutube'],
+      blogs = ['wordpress', 'livejuornal', 'bloggers'],
+      internet = [...video, ...blogs, 'vk', 'facebook'];
+
+console.log(internet);
+
+function log(a, b, c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
 }
 
-const str = prompt('', '');
-const product = str.split(", ");
-product.sort();
-console.log(product.join("; "));
+const num = [2, 5, 7];
+
+log(...num);
+
+const array = ['a', 'b'];
+const newAarray = [...array];
+
+const q = {
+    one: 1,
+    two: 2
+};
+
+const newQ = {...q};
+console.log(newQ);
